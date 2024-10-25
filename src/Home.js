3650b00c848ee2e237d83ed7d087caff95e1d01c
lleusxam <blkleusxam@hotmail.com>
2024-10-24 21:26:52 -0300

    Primeiro commit

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Home() {
+    return(
+        <div>
+            <h1>Estamos trabalhando aqui.</h1>
+        </div>
+    );
+}
+
+export default Home;
