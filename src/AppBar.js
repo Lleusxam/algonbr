aa36d2d17491506c09ed8130e81d980580ade388
lleusxam <blkleusxam@hotmail.com>
2024-10-25 22:49:02 -0300

    Adição de fonte personalizada

--- src/AppBar.js
+++ src/AppBar.js
@@ -5,20 +5,20 @@ const AppBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">algon</Link>
+        <Link className="navbar-brand titulo" to="/" style={{fontWeight: 'bold', fontSize: 26}}>algon</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
+              <Link className="nav-link titulo" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">Sobre</Link>
+              <Link className="nav-link titulo" to="/about">Sobre</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contato</Link>
+              <Link className="nav-link titulo" to="/contact">Contato</Link>
             </li>
           </ul>
         </div>
